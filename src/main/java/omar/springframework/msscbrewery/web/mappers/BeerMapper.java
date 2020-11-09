044f05f9a62886c01a647a565e348120c61955d6
Omar Morales <morales.ortega.omar@gmail.com>
2020-11-08 21:49:17 -0600

    MapStruct example

---
+++ src/main/java/omar/springframework/msscbrewery/web/mappers/BeerMapper.java
@@ -0,0 +1,12 @@
+package omar.springframework.msscbrewery.web.mappers;
+
+import omar.springframework.msscbrewery.domain.Beer;
+import omar.springframework.msscbrewery.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+    Beer beerDtoToBeer(BeerDto dto);
+}
